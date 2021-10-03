1894c90275132f4801487af807868c48224800b0
RizkyHendraSaputra <82431281+RizkyHendraSaputra@users.noreply.github.com>
2021-10-03 13:14:26 +0700

    Push Cloud

---
+++ Assets/Scripts/AnalyticsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Analytics;   
+
+public class AnalyticsManager 
+{
+   private static void LogEvent(string eventName, params Parameter[]parameters)
+    {
+        // untuk method utama untuk menembakan firebase
+        FirebaseAnalytics.LogEvent(eventName, parameters);
+    }
+    public static void LogUpgradeEvent (int resourceIndex, int level)
+    {
+        // untuk kita memakai event dan parameter yang tersedia di firebase(tidak memakai yang custom)
+        // untuk agar dapat muncul sebagai report data di analityc firebase
+        LogEvent(
+            FirebaseAnalytics.EventLevelUp,
+            new Parameter(FirebaseAnalytics.ParameterIndex, resourceIndex.ToString()),
+            new Parameter(FirebaseAnalytics.ParameterLevel, level));
+        // untuk karena resourceindex digunakan sebagai id, maka seharusnya kita menyimpannya sebagai string bukan integer
+    }
+    public static void LogUnlockEvent(int resourceIndex)
+    {
+        LogEvent(
+            FirebaseAnalytics.EventUnlockAchievement, new Parameter(FirebaseAnalytics.ParameterIndex, resourceIndex.ToString()));   
+    }
+    public  static void SetUserProperties (string name, string value)
+    {
+        FirebaseAnalytics.SetUserProperty(name, value);
+    }
+}
--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿    using UnityEngine;
 using UnityEngine.UI;
 
 public class ResourceController : MonoBehaviour
@@ -19,7 +19,7 @@ public class ResourceController : MonoBehaviour
         {
             // untuk menyimpan value yang di set ke _level pada progress data
             UserDataManager.Progress.ResourcesLevels[_index] = value;
-            UserDataManager.Save();
+            UserDataManager.Save(true);
         }
         get
         {
@@ -93,6 +93,7 @@ public class ResourceController : MonoBehaviour
 
         ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
         ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        AnalyticsManager.LogUpgradeEvent(_index, _level);
     }
 
     public void UnlockResource ()
@@ -106,6 +107,7 @@ public class ResourceController : MonoBehaviour
         SetUnlocked (true);
         GameManager.Instance.ShowNextResource ();
         AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
+        AnalyticsManager.LogUnlockEvent(_index);
     }
 
     public void SetUnlocked (bool unlocked)
@@ -117,7 +119,7 @@ public class ResourceController : MonoBehaviour
             if(!UserDataManager.HasResources(_index))
             {
                 UserDataManager.Progress.ResourcesLevels.Add(_level);
-                UserDataManager.Save();
+                UserDataManager.Save(true);
             }
         }
         IsUnlocked = unlocked;
